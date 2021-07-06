33eddf8103699f215483ea272fd4d0c216d6f64f
xipian123 <85778182+xipian123@users.noreply.github.com>
2021-07-07 00:37:38 +0800

    Add files via upload

--- xiaole.js
+++ xiaole.js
@@ -67,9 +67,9 @@ let money;
 $.message = '', $.messagesss = '', $.index = '', $.clocklog = '', COOKIES_SPLIT = '';
 
 const xiaoleurlArr = [];
-let xiaoleurlVal = ``;
+let xiaoleurlVal = `https://minapp.xqrobot.net/user.php?mod=index&client=minapp&version=2.7&site=yyq&user_openid=ofGEO5Aldk3wqyljR_MD3qxGnb44&user_lbs=&city=&u=11084`;
 const xiaoleheaderArr = [];
-let xiaoleheaderVal = ``;
+let xiaoleheaderVal = `{"Cookie":"PHPSESSID=05vogph23np6om345uet00h2t2","Accept":"*/*","Connection":"keep-alive","Referer":"https://servicewechat.com/wx7d27c26991711f37/157/page-frame.html","Content-Type":"application/json","Host":"minapp.xqrobot.net","User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.2(0x18000223) NetType/WIFI Language/zh_CN","Accept-Encoding":"gzip, deflate, br","Accept-Language":"zh-cn"}`;
 let middlexiaoleURL = [];
 let middlexiaoleHEADER = [];
 
